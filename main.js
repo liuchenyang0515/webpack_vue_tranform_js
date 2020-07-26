099979d73a3f22102812a39f784851c274574920
liuchenyang <897418643@qq.com>
2020-07-26 23:14:52 +0800

    增加babel语法转换配置

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 import vue from 'vue'
 import vueRouter from 'vue-router'
 import Comp from './view/comp.vue'
-
+// =========== 测试babel语法转换
+let a = (a, b)=>{}
+new Promise((resolve)=>{
+    setTimeout(() => {
+        console.log(123123);
+        resolve();
+    }, 2000);
+});
+// ===========
 new vue({
     render : h=>h(Comp)
 }).$mount('#app')
